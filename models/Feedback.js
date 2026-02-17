a9cc624ed0ad81e245fa1d2cccbb602ba9d1e080
sharansh nayak <nayaksharansh@gmail.com>
2026-02-17 23:42:17 +0530

    feedback rating average added

--- models/Feedback.js
+++ models/Feedback.js
@@ -57,7 +57,7 @@ const feedbackSchema = new mongoose.Schema({
   },
   feedbackRating: {
     type: String,
-    enum: ['', 'poor', 'avergae' 'good', 'very good', 'excellent']
+    enum: ['', 'poor', 'average', 'good', 'very good', 'excellent']
   },
   totalFeedbacks: {
     type: Number,
